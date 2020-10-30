618452a83b946cbfe7d4b4e212afde6ed79a2274
Maxondria <maxtayebwa@gmail.com>
2020-10-30 23:45:02 +0300

    Make queries, pass args, etc

--- src/components/People/index.jsx
+++ src/components/People/index.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Person from "./compoents/Person";
+
+const fetchPlanets = async () => {
+  const res = await fetch("http://swapi.dev/api/people/");
+  return res.json();
+};
 
 const People = () => {
+  const { data, isError, isFetching, isLoading, isSuccess } = useQuery(
+    ["people"],
+    fetchPlanets
+  );
+
   return (
     <div>
       <h2>People</h2>
+      {isError && <div> Error fetching data!! </div>}
+      {isLoading && <div> Loading... </div>}
+      {isFetching && !isLoading && <div> Updating... </div>}
+      {isSuccess && data && (
+        <div>
+          {data.results.map((people) => (
+            <Person key={people.name} person={people} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Planets/index.jsx
+++ src/components/Planets/index.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import Planet from "./components/Planet";
+
+const fetchPlanets = async (_, page) => {
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
+  return res.json();
+};
 
 const Planets = () => {
+  const [page, setPage] = useState(1);
+  const { data, isError, isFetching, isLoading, isSuccess } = useQuery(
+    ["planets", page],
+    fetchPlanets,
+    {
+      onSuccess: () => console.log("You can do something awesome!."),
+    }
+  );
+
   return (
     <div>
       <h2>Planets</h2>
+
+      <button onClick={() => setPage(page + 1)}>Page {page}</button>
+
+      {isError && <div> Error fetching data </div>}
+      {isLoading && <div> Loading... </div>}
+      {isFetching && !isLoading && <div> Updating... </div>}
+      {isSuccess && data && (
+        <div>
+          {data.results.map((planet) => (
+            <Planet key={planet.name} planet={planet} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
